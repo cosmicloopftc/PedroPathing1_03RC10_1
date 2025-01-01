355bf024f0b33ea3fca435c4c152e2ef756e380f
baronclaps <barbarhendo180@gmail.com>
2024-12-31 18:24:47 -0600

    1/2/2025: OT: start to add in L and F constants, add template for OTOS
    Update the Quickstart to v1.0.3 of Pedro:
    
    - Update dependency to 1.0.3
    - Update the Tuners and Examples to match the new method of updating the Constants.
    
    Note: Now users will have to call `Constants.setConstants(FConstants.class, LConstants.class);` before initalizing their follower (`follower = new Follower(hardwareMap);`).

--- TeamCode/src/main/java/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -4,7 +4,12 @@ import com.pedropathing.localization.Localizers;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.util.CustomFilteredPIDFCoefficients;
 import com.pedropathing.util.CustomPIDFCoefficients;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class FConstants {
     static {
@@ -15,38 +20,42 @@ public class FConstants {
         FollowerConstants.rightFrontMotorName = "rightFront";
         FollowerConstants.rightRearMotorName = "rightRear";
 
-        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
-        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
-        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
-        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;    //TODO
+        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;     //TODO
+        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;   //TODO
+        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;    //TODO
 
-        FollowerConstants.mass = 13;
+        FollowerConstants.mass = 12.2;      // default 13;
 
-        FollowerConstants.xMovement = 57.8741;
-        FollowerConstants.yMovement = 52.295;
+        FollowerConstants.xMovement = 58;   // default 57.8741;     //v1=60; section 4.1 ForwardVelocityTuner 12/15/2024, battery V=13.6
+        FollowerConstants.yMovement = 46;//TODO   // default 52.295;      //V1=46; section 4.2 Strafe VelocityTuner 12/15/2024, battery V=13.6
 
-        FollowerConstants.forwardZeroPowerAcceleration = -41.278;
-        FollowerConstants.lateralZeroPowerAcceleration = -59.7819;
+        FollowerConstants.forwardZeroPowerAcceleration = -41.278;   //TODO  //V1=-37; section 5.1 forwardZeroPowerAccelerationTuner 12/15/2024, battery V=13.5
+        FollowerConstants.lateralZeroPowerAcceleration = -59.7819;  //TODO //V1=-65 (variable -63 to -68)  section 5.2 lateralZeroPowerAccelerationTuner 12/15/2024, battery V=13.5
 
-        FollowerConstants.translationalPIDFCoefficients = new CustomPIDFCoefficients(0.1,0,0.01,0);
+        FollowerConstants.translationalPIDFCoefficients = new CustomPIDFCoefficients(0.1,0,0.01,0); //TODO //V1=0.41,0,0.050,0;
         FollowerConstants.useSecondaryTranslationalPID = false;
-        FollowerConstants.secondaryTranslationalPIDFCoefficients = new CustomPIDFCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
+        FollowerConstants.secondaryTranslationalPIDFCoefficients = new CustomPIDFCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID  //TODO //V1=0.2,0,0.02,0;
 
-        FollowerConstants.headingPIDFCoefficients = new CustomPIDFCoefficients(2,0,0.1,0);
+        FollowerConstants.headingPIDFCoefficients = new CustomPIDFCoefficients(2,0,0.1,0);   //TODO //V1=2,0,0.02,0
         FollowerConstants.useSecondaryHeadingPID = false;
-        FollowerConstants.secondaryHeadingPIDFCoefficients = new CustomPIDFCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
+        FollowerConstants.secondaryHeadingPIDFCoefficients = new CustomPIDFCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID   //TODO //V1=4,0,0.01,0
 
-        FollowerConstants.drivePIDFCoefficients = new CustomFilteredPIDFCoefficients(0.1,0,0,0.6,0);
+        FollowerConstants.drivePIDFCoefficients = new CustomFilteredPIDFCoefficients(0.1,0,0,0.6,0);    //TODO //V1=0.01,0,0.000001,0.6,0
         FollowerConstants.useSecondaryDrivePID = false;
-        FollowerConstants.secondaryDrivePIDFCoefficients = new CustomFilteredPIDFCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
+        FollowerConstants.secondaryDrivePIDFCoefficients = new CustomFilteredPIDFCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID  //TODO //V1=0.009,0.000005,0.6,0
 
-        FollowerConstants.zeroPowerAccelerationMultiplier = 4;
-        FollowerConstants.centripetalScaling = 0.0005;
+        FollowerConstants.zeroPowerAccelerationMultiplier = 4;      //TODO //V1=4
+        FollowerConstants.centripetalScaling = 0.0005;              //TODO //V1=0.000020
 
-        FollowerConstants.pathEndTimeoutConstraint = 500;
-        FollowerConstants.pathEndTValueConstraint = 0.995;
-        FollowerConstants.pathEndVelocityConstraint = 0.1;
+        FollowerConstants.pathEndTimeoutConstraint = 500;           //V1=500
+        FollowerConstants.pathEndTValueConstraint = 0.995;          //V1=0.995
+        FollowerConstants.pathEndVelocityConstraint = 0.1;          //V1=0.1
         FollowerConstants.pathEndTranslationalConstraint = 0.1;
-        FollowerConstants.pathEndHeadingConstraint = 0.007;
+        FollowerConstants.pathEndHeadingConstraint = 0.007;         //V1=0.007
+
+
+
+
     }
 }
