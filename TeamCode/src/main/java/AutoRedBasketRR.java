e1f5752e29f5df3c2a124aa729ec04ee0c14ff9a
oktra <oliverkotran@gmail.com>
2025-01-10 22:44:32 -0500

    1/5/2025: MT--tune claw servo and outtake extension to be done: Axon outtake arm
    Merge remote-tracking branch 'origin/master_Jan3latest' into master_Jan3latest

--- TeamCode/src/main/java/AutoRedBasketRR.java
+++ TeamCode/src/main/java/AutoRedBasketRR.java
@@ -1,10 +1,16 @@
+import androidx.annotation.NonNull;
+
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import Hardware.HardwareNoDriveTrainRobot;
 import RR.MecanumDrive;
 
 @Config
@@ -13,54 +19,117 @@ import RR.MecanumDrive;
 public final class AutoRedBasketRR extends LinearOpMode {
     public static double DISTANCE = 64;
 
+
+
     @Override
     public void runOpMode() throws InterruptedException {
 
         Pose2d initialPose = new Pose2d(-32, -62, Math.toRadians(0));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        HardwareNoDriveTrainRobot autoRobot = new HardwareNoDriveTrainRobot();
+
+
+
         waitForStart();
 
-        Actions.runBlocking(
-                drive.actionBuilder(new Pose2d(-32, -62, Math.toRadians(0)))
-                //    .lineToY(-33)
-                        .setTangent(45)
-                    .splineToLinearHeading(new Pose2d(-54, -59, Math.toRadians(45)), Math.toRadians(0))// score preload
-                    .build());
-                   //     .lineTo
-//                    .lineToY(-45)
-//                        .setTangent(Math.toRadians(0))
-//                    .lineToX(20)
-//                        .setTangent(Math.toRadians(90))
-//                    .lineToY(-16)
-//                        .setTangent(Math.toRadians(0))
-//                    .lineToX(30)
-//                        .setTangent(Math.toRadians(90))
-//                    .lineToY(-57)  // deliver sample1
-//                    .lineToY(-16)
-//                        .setTangent(Math.toRadians(0))
-//                    .lineToX(39)
-//                        .setTangent(Math.toRadians(90))
-//                    .lineToY(-57)  // deliver sample2
-//                    .lineToY(-16)
-//                        .setTangent(Math.toRadians(0))
-//                    .lineToX(43)
-//                        .setTangent(Math.toRadians(90))
-//                    .lineToY(-57)  // deliver sample3
-//                    .lineToY(-60)  // grab spec1
-//                    .splineToConstantHeading(new Vector2d(0, -32), Math.toRadians(90))  // score spec1
-//                    .splineToConstantHeading(new Vector2d(30, -57), Math.toRadians(90))  // pre-pickup spec2
-//                        .lineToY(-60)  // grab spec2
-//                        .splineToConstantHeading(new Vector2d(0, -32), Math.toRadians(90))  // score spec2
-//                        .splineToConstantHeading(new Vector2d(30, -57), Math.toRadians(90)) // pre-pickup spec2
-//                        .lineToY(-60) // grab spec 2
-//                        .splineToConstantHeading(new Vector2d(0, -32), Math.toRadians(90)) // score spec2
-//                        .splineToConstantHeading(new Vector2d(30, -57), Math.toRadians(90)) // pre-pickup spec3
-//                        .lineToY(-60)  // grab spec3
-//                        .splineToConstantHeading(new Vector2d(0, -32), Math.toRadians(90))  // score spec3
 
-                   // .lineToX(30)
+
+//        Actions.runBlocking(
+//                drive.actionBuilder(new Pose2d(-32, -62, Math.toRadians(0)))
+//                //    .lineToY(-33)
+//                        .setTangent(45)
+//                    .splineToLinearHeading(new Pose2d(-54, -59, Math.toRadians(45)), Math.toRadians(0))// score preload
+//                    .build());
+
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(-32,-62,Math.toRadians(0)))
+                        .setTangent(45)
+                        .strafeToSplineHeading(new Vector2d(-54, -59), Math.toRadians(45))
+
+                   //     .setTangent(0)
+                        //outtake preload
+                        .turnTo(Math.toRadians(79))
+                        //intake
+                        .turnTo(Math.toRadians(45))
+                        //outtake
+                        .turnTo(Math.toRadians(100))
+                        //intake
+                        .turnTo(Math.toRadians(45))
+                        //outtake
+                        .turnTo(Math.toRadians(120))
+                        //intake
+                        .turnTo(Math.toRadians(45))
+                        //outtake
+                        .build());
 
 
 
     }
+
+    /**to implementing this:
+     *         Actions.runBlocking(
+     *                 new SequentialAction(
+     *                         trajectoryActionChosen,
+     *                         AutoOuttakeSliderHighBasket(),
+     *                         trajectory....
+     *                         .waitSeconds(3)
+     *                         AutoOuttakeSliderHighBasket(),
+     *                         trajectoryActionCloseOut
+     *                 )
+     *         );
+     *
+     */
+    public class AutoOuttakeSlider  {
+        HardwareNoDriveTrainRobot autoRobot = new HardwareNoDriveTrainRobot();
+        public class AutoOuttakeSliderHighBasket implements Action {
+            private boolean initialized = false;
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    autoRobot.Outtake.leftSlideSetPositionPower(3400,0.5);
+                    autoRobot.Outtake.rightSlideSetPositionPower(3400,0.5);
+                    initialized = true;
+                }
+                double positionOuttakeLeftSlide = autoRobot.Outtake.outtakeLeftSlide.getCurrentPosition();
+                packet.put("Outtake slider-left position", positionOuttakeLeftSlide);
+                if (positionOuttakeLeftSlide < 3400.0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        public Action AutoOuttakeSliderHighBasket() {
+            return new AutoOuttakeSliderHighBasket();
+        }
+
+
+        public class AutoOuttakeSliderReadyPosition implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    autoRobot.Outtake.leftSlideSetPositionPower(500,0.5);
+                    autoRobot.Outtake.rightSlideSetPositionPower(500,0.5);
+                    initialized = true;
+                }
+                double positionOuttakeLeftSlide = autoRobot.Outtake.outtakeLeftSlide.getCurrentPosition();
+                packet.put("Outtake slider-left position", positionOuttakeLeftSlide);
+
+                if (positionOuttakeLeftSlide > 500.0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        public Action AutoOuttakeSliderReadyPosition(){
+            return new AutoOuttakeSliderReadyPosition();
+        }
+    }
+
+
+
+
 }
