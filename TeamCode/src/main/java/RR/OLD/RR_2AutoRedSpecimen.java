1f8a875f65e1ea5c1021768c2e37bf3dcf05f34c
oktra <oliverkotran@gmail.com>
2025-02-16 12:52:02 -0500

    2/16: Pedro_AutoRedSpecimen1 done with moves
    2/16: version 3: MT: reactivate color sensor again, inside Telemetry color method, change to red, blue, yellow value and not reading eachtime.
      WT: update Pedro AutoRedSpecimen1 to have curvaPath ver1
    
    2/16: version 2: MT/WT: move robot.Sensor.colorIntake.setGain(colorGain) to init() only
    
    2/16: MT move led program into telemetry method to attempt to fix lag
    WT: update to pedro pathing 1.0.8
    1.17.2025: Dom, oliver auto
    1/16/2025: OT start with Auto--use intake to take sample to home
    
    1/13/2025: MT update teleOp
               OT--path test of RedSpec
    
    1/11/2025: OT testing method in AutoCore vs. using LinearOp mode directed with extending a common program
    
    1/6/2025 MT: Axon outtake arm setup
    1/5/2025: MT--tune claw servo and outtake extension
    to be done: Axon outtake arm
    
    Merge remote-tracking branch 'origin/master_Jan3latest' into master_Jan3latest

--- TeamCode/src/main/java/RR/OLD/RR_2AutoRedSpecimen.java
+++ TeamCode/src/main/java/RR/OLD/RR_2AutoRedSpecimen.java
@@ -1,9 +1,8 @@
-package RR;
+package RR.OLD;
 
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.AccelConstraint;
@@ -17,13 +16,13 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.VelConstraint;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.pedropathing.util.Timer;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import Hardware.HardwareNoDriveTrainRobot;
+import RR.MecanumDrive;
 
 //@Config
 //@Autonomous(name = "RR_2AutoRedSpecimen v1.1", group = "Auto")
